f75272edaa279409bd130c5a7ee9b1f5eaad1142
Ashutosh <ashutoshdhondkar@gmail.com>
2022-12-12 01:46:56 +0530

    modified algo, failing few test cases, need rework

--- closest_value_in_bst/main.py
+++ closest_value_in_bst/main.py
@@ -42,35 +42,43 @@ def construct(arr):
             _ = place_current_node_at_appropriate_pos(root, new_node)
     return root
 
-def closestValue(tree, target):
-    if tree is None:
-        return False
+def findClosestValueInBst(tree, target):
+    if tree == None:
+        return None
     parent_dist, left_dist, right_dist = -1, 10000, 10000
-    parent_dist = tree.value - target
+    parent_dist = abs(tree.value - target)
     
     if tree.left:
-        left_dist = tree.left.value - target
+        left_dist = abs(tree.left.value - target)
     if tree.right:
-        right_dist = tree.right.value - target
+        right_dist = abs(tree.right.value - target)
     
-    min_dist, child = (left_dist, "left") if left_dist < right_dist else (right_dist, "right")
-    if parent_dist < min_dist:
-        return tree.value
+    child = "left" if left_dist < right_dist else "right"
     if child == "left":
-        resp = closestValue(tree.left, target)
+        returned_elem = findClosestValueInBst(tree.left, target)
     if child == "right":
-        resp = closestValue(tree.right, target)
-    if resp == False:
+        returned_elem = findClosestValueInBst(tree.right, target)
+    if returned_elem is None:
         return tree.value
-    return resp
+    if abs(returned_elem-target) <parent_dist:
+        return returned_elem
+    return tree.value
+    # if returned_dist < parent_dist:
+    #     return returned_dist, returned_elem
+    # else:
+    #     return parent_dist, tree.value
+    
+
 
 cases = [
     (([10, 7, 14, 6, 8, 13, 15, 3, 5],2), 3),
     (([10, 5, 15, 2, 5, 13, 22, 1, 14], 12), 13)
 ]
 
+
+
 for case in cases:
     arguments = case[0]
     root = construct(arguments[0])
-    res = closestValue(root, target=arguments[1])
+    res = findClosestValueInBst(root, target=arguments[1])
     print(res, case[1])

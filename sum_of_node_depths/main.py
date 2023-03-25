af320a16c0c8e274d5864592a48fb2ae6b1aa2ad
Ashutosh <ashutoshdhondkar@gmail.com>
2023-03-25 17:04:40 +0530

    fixed -1 as default value instead of 0

--- sum_of_node_depths/main.py
+++ sum_of_node_depths/main.py
@@ -1,15 +1,16 @@
 from tree_utils import construct_example_tree
 
 def nodeDepths(root):
-    return nodeDepthUtil(root, currentDepth=-1, depth_sum=0)
+    return nodeDepthUtil(root, currentDepth=0, depth_sum=0)
 
 
 def nodeDepthUtil(node, currentDepth, depth_sum):
     if node is None:
         return depth_sum
 
-    currentDepth += 1
+    print(node.value, currentDepth, depth_sum)
     depth_sum += currentDepth
+    currentDepth += 1
     depth_sum = nodeDepthUtil(node.left, currentDepth, depth_sum)
     depth_sum = nodeDepthUtil(node.right, currentDepth, depth_sum)
     return depth_sum

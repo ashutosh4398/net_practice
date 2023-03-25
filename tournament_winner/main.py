2f7ee39bccec7c06975c2821c1207cf3701ca0c5
Ashutosh <ashutoshdhondkar@gmail.com>
2023-03-25 12:57:55 +0530

    tournament winner solution

---
+++ tournament_winner/main.py
@@ -0,0 +1,29 @@
+def tournamentWinner(competitions, results):
+    # each array in competitions is of the form: [home_team, away_team]
+    # if results[i] = 1 => home team won
+    # if results[i] = 0 => away team won
+
+    points_table = {}
+    for (competition, result) in zip(competitions, results):
+        (winner, loser) = (competition[0], competition[1]) if result == 1 else (competition[1], competition[0])
+        
+        points_table[winner] = points_table.get(winner,0) + 3
+        points_table[loser] = points_table.get(loser,0)
+
+    final_winner, max_points = None, 0
+    for (winner,points) in points_table.items():
+        if points > max_points: 
+            final_winner,max_points = winner, points
+    
+    return final_winner
+
+
+competitions = [
+    ["HTML", "Java"],
+    ["Java", "Python"],
+    ["Python", "HTML"]
+]
+
+results = [0,1,1]
+
+print(tournamentWinner(competitions, results))

43eb44a963ac8f54a2b2e3705f3e970157ecf5a8
Ashutosh <ashutoshdhondkar@gmail.com>
2023-03-25 13:10:50 +0530

    updated code with only one loop

--- tournament_winner/main.py
+++ tournament_winner/main.py
@@ -4,18 +4,24 @@ def tournamentWinner(competitions, results):
     # if results[i] = 0 => away team won
 
     points_table = {}
+    winner_team, max_points = None, 0
     for (competition, result) in zip(competitions, results):
+
+        # updating points table
         (winner, loser) = (competition[0], competition[1]) if result == 1 else (competition[1], competition[0])
-        
         points_table[winner] = points_table.get(winner,0) + 3
         points_table[loser] = points_table.get(loser,0)
 
-    final_winner, max_points = None, 0
-    for (winner,points) in points_table.items():
-        if points > max_points: 
-            final_winner,max_points = winner, points
+        # current winning team so far
+        if points_table[winner] > max_points:
+            winner_team, max_points = winner, points_table[winner]
+
+    # final_winner, max_points = None, 0
+    # for (winner,points) in points_table.items():
+    #     if points > max_points: 
+    #         final_winner,max_points = winner, points
     
-    return final_winner
+    return winner_team
 
 
 competitions = [

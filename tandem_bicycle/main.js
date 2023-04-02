e94630541001119fb472def7d8b35ee371961690
Ashutosh <ashutoshdhondkar@gmail.com>
2023-04-02 12:53:34 +0530

    solved problem in both python and javascript

---
+++ tandem_bicycle/main.js
@@ -0,0 +1,39 @@
+const ASCENDING = 1;
+const DESCENDING = 0;
+
+const compareFunction = (order) => {
+  return order === ASCENDING
+    ? (a, b) => a - b
+    : order === DESCENDING
+    ? (a, b) => b - a
+    : (a, b) => 0;
+};
+
+function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+  // Write your code here.
+  redShirtSpeeds.sort(compareFunction(ASCENDING));
+
+  // to achieve max possible fastest, sort the arrays in opposite direction
+  // to achieve min possible fastest, sort the arrays in same direction
+  const blueOrdering = fastest ? DESCENDING : ASCENDING;
+  blueShirtSpeeds.sort(compareFunction(blueOrdering));
+
+  // iterate over both arrays
+  let totalSpeed = redShirtSpeeds
+    .map((redSpeed, index) => {
+      const blueSpeed = blueShirtSpeeds[index];
+      return Math.max(redSpeed, blueSpeed);
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  return totalSpeed;
+}
+
+const redShirtSpeeds = [5, 5, 3, 9, 2];
+const blueShirtSpeeds = [3, 6, 7, 2, 1];
+const fastest = true;
+
+console.log(tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest));
+
+// Do not edit the line below.
+exports.tandemBicycle = tandemBicycle;
---
+++ tandem_bicycle/main.py
@@ -0,0 +1,20 @@
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    redShirtSpeeds.sort()
+    if fastest:
+        blueShirtSpeeds.sort(reverse = True)
+    else:
+        blueShirtSpeeds.sort()
+
+    totalSpeed = 0
+    for (redSpeed, blueSpeed) in zip(redShirtSpeeds, blueShirtSpeeds):
+        totalSpeed += max(redSpeed, blueSpeed)    
+    # Write your code here.
+    return totalSpeed
+
+
+
+redShirtSpeeds = [5,5,3,9,2]
+blueShirtSpeeds = [3,6,7,2,1]
+fastest = True
+
+print(tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest))

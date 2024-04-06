16ed9c91d0dab32e85d2cefcaeb2a26237382601
ashutosh4398 <ashutoshdhondkar@gmail.com>
2024-04-06 20:46:38 +0530

    done with binary subarrays problem

---
+++ binary_subarrays_with_sum.py
@@ -0,0 +1,55 @@
+"""
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+nums[i] is either 0 or 1.
+0 <= goal <= nums.length
+
+"""
+
+from typing import List
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def helper(x):
+            if x < 0: return 0
+            currSum = 0
+            result = 0
+            l = 0
+            for r in range(len(nums)):
+                currSum += nums[r]
+                while currSum > x:
+                    currSum -= nums[l]
+                    l += 1
+                result += (r - l + 1)
+            return result
+        return helper(goal) - helper(goal-1)
+
+def main():
+    s = Solution()
+    print(s.numSubarraysWithSum([0,0,1,0,1,0,1,0], 1))
+
+
+if __name__ == "__main__":
+    main()

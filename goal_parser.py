18766405d3a62089d0b2ac921e4dab73675b7653
ashutosh4398 <ashutoshdhondkar@gmail.com>
2025-03-15 22:38:37 +0530

    streak day 2

---
+++ goal_parser.py
@@ -0,0 +1,47 @@
+"""
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+ 
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+ 
+
+Constraints:
+
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order.
+"""
+
+class Solution:
+    def interpret(self, command: str) -> str:
+        words = {"G": "G", "()": "o", "(al)": "al"}
+        index = 0
+        result = []
+        for i in range(len(command)):
+            if command[index: i+1] in words:
+                result.append(words[command[index: i+1]])
+                index = i+1
+        return "".join(result)
+
+s = Solution()
+print(s.interpret("G()(al)"))
+print(s.interpret("G()()()()(al)"))
+print(s.interpret("(al)G(al)()()G"))
